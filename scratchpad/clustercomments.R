548cc96946f003b9c7dac7c1cb2445b05f37f27d
judgelord <devin.jl@gmail.com>
2019-01-07 17:47:48 -0600

    2 topic lda loop

--- scratchpad/clustercomments.R
+++ scratchpad/clustercomments.R
@@ -1,48 +1,101 @@
-install.packages(c("topicmodels", "tidytext", "tidyverse"))
 
-install.packages("quanteda")
-library(topicmodels)
-library(tidyverse)
-library(tidytext)
-library(quanteda)
-library(tm)
-
-
-data("AssociatedPress")
-AssociatedPress
-
-
-text_df <- data_frame(document = 1:4338, text = all$commentText)
-
-text_df %<>% 
-  filter(!grepl("See attached", text)) %>% 
-  unnest_tokens(word, text)
-
-tidy(text_df)
-
-cast_dtm(text_df, document = document, term = word, value = word)
-
-ap_lda <- LDA(cast_dtm(text_df, word, document, value = word), 
-              k = 2, control = list(seed = 1234))
-ap_lda
+source(here("setup.R"))
+#####################################
+# select and name documentID and text columns
+text_df <- select(mass, document = documentId, text = commentText, group = docketId) %>%
+  # clean up and tokenize
+  mutate(text = tolower(text)) %>% 
+  # filter out whole TEXTS with certian phrases or terms
+  filter(!grepl("see attached|attachment", text)) %>%
+  # tokenize by word, new column is called word
+  unnest_tokens(word, text) %>%
+  # filter out unwanted WORDS 
+  anti_join(get_stopwords()) %>%
+  filter(!word %in% c("see", "attach.*", "comment.*", "federal", "agenc.*") ) %>% 
+  filter(!is.na(word))
 
 
+# loop over groups (e.g. dockets) modeling each 
+for(i in "BLM-2013-0002"){ #text_df$group){
+  text_df %<>% 
+    filter(group == i) %>% 
+    group_by(document, word) %>% 
+    summarise(count = n()) %>% 
+    ungroup() 
+}
 
-ap_topics <- tidy(ap_lda, matrix = "beta")
-ap_topics
+## make document term matrix
+## Turns a tidy one-term-per-document-per-row data frame into a DocumentTermMatrix
+cast_dtm(text_df, document = document, term = word, value = count)
+
+lda <- LDA(cast_dtm(text_df, document = document, term = word, value = count), 
+           k = 2, control = list(seed = 1234))
+
+# # Beta matrix = dist of words over topics
+# select top words in each topic 
+topic_names <- tidy(lda, matrix = "beta") %>%
+  group_by(topic) %>%
+  top_n(10, beta) %>%
+  summarise(name = paste(term, collapse = ", ")) %>% 
+  ungroup() 
+  
+# Gamma matrix = dist of topics over documents 
+topics <- tidy(lda, matrix = c("gamma")) %>% 
+  left_join(topic_names)
+# topics %<>% spread(topic, gamma)
+
+topics$group <- group
+
+topics %<>% rename(documentId = document)
+
+topics %<>% left_join(mass)
 
 
 
-ap_top_terms <- ap_topics %>%
+# For two topics 
+topics$topic <- topics$topic-1
+topics %>% 
+  mutate(gamma =ifelse(topic==1,gamma,NA)) %>% 
+ggplot() + 
+  geom_point(aes(x = postedDate,y = gamma, size = numberOfCommentsReceived), alpha = .2) + 
+  geom_text(aes(x = postedDate, y = topic, label = name), check_overlap = T, size = 2, color ="blue")+
+  geom_text(aes(x = postedDate, y = gamma, label = organization), size =1) +
+  facet_grid(. ~ group)
+##
+
+
+
+
+
+
+
+
+
+
+# Beta matrix = dist of words over topics 
+topics <- tidy(lda, matrix = "beta")
+
+top_terms <- topics %>%
   group_by(topic) %>%
   top_n(10, beta) %>%
   ungroup() %>%
   arrange(topic, -beta)
 
-ap_top_terms %>%
+top_terms %>%
   mutate(term = reorder(term, beta)) %>%
   ggplot(aes(term, beta, fill = factor(topic))) +
   geom_col(show.legend = FALSE) +
   facet_wrap(~ topic, scales = "free") +
   coord_flip()
+
+
+
+
+
+# k means 
+
+
+
+
+
 
--- setup.R
+++ setup.R
@@ -2,21 +2,27 @@ options(stringsAsFactors = FALSE)
 
 requires <- c("dplyr", 
               "ggplot2", 
-              "gdata", 
               "magrittr",
-              "stringi",
               "stringr", 
-              "stargazer",
               "here",
-              "tidyverse")
+              "tidyverse",
+              "topicmodels",
+              "tidytext",
+              "quanteda",
+              "tm")
 to_install <- c(requires %in% rownames(installed.packages()) == FALSE)
 install.packages(c(requires[to_install], "NA"), repos = "https://cloud.r-project.org/" )
 rm(requires, to_install)
 
 library(tidyverse)
 library(dplyr) # in case tydyverse fails (problem on linux)
-library(ggplot2)
+library(ggplot2); theme_set(theme_bw())
 library(magrittr)
 library(stringr)
-library(stringi)
 library(here)
+
+library(topicmodels)
+library(tidyverse)
+library(tidytext)
+library(quanteda)
+library(tm)
