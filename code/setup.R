c0601f977824e040c4765b7d2a1cf1ead6ec2de1
judgelord <devin.jl@gmail.com>
2021-04-14 19:57:07 -0500

    add hand-coded results

--- code/setup.R
+++ code/setup.R
@@ -10,12 +10,28 @@ install.packages(c(requires[to_install], "NA"), repos = "https://cloud.r-project
 rm(requires, to_install)
 
 library(tidyverse)
-library(ggplot2); theme_set(theme_bw())
+# library(ggplot2); theme_set(theme_bw())
 library(magrittr)
 library(broom)
 library(here)
 library(msm)
 library(kableExtra)
+
+library(ggplot2); theme_set(theme_minimal())
+options(
+  ggplot2.continuous.color = "viridis",
+  ggplot2.continuous.fill = "viridis"
+)
+scale_color_discrete <- function(...)
+  scale_color_viridis_d(...)
+scale_fill_discrete <- function(...)
+  scale_fill_viridis_d(...)
+
+kablebox <- . %>% 
+  head(100) %>%
+  knitr::kable() %>% 
+  kable_styling() %>% 
+  scroll_box(height = "400px")
 
 
 ## Sets defaults for R chunks
@@ -24,18 +40,47 @@ knitr::opts_chunk$set(echo = FALSE, # echo = TRUE means that your code will show
                       message = FALSE,
                       fig.show="hold",
                       fig.pos= "htbp",
-                      out.extra = "",
                       fig.path = "figs/",
-                      out.width = "100%",
                       fig.align='center',
-                      fig.cap = '...',
+                      fig.cap = '   ',
                       fig.retina = 6,
                       fig.height = 3,
-                      fig.width = 7)
+                      fig.width = 7,
+                      out.width = "100%",
+                      out.extra = "")
 
 options(stringsAsFactors = FALSE)
 
 options(knitr.graphics.auto_pdf = TRUE)
+
+
+#functions for case sensitive string manipulation
+str_rm_all <- function(string, pattern) {
+  str_remove_all(string, regex(pattern, ignore_case = TRUE))
+}
+
+str_rpl <- function(string, pattern, replacement) {
+  str_replace(string, regex(pattern, ignore_case = TRUE), replacement)
+}
+
+str_rm <- function(string, pattern) {
+  str_remove(string, regex(pattern, ignore_case = TRUE))
+}
+
+str_dct <- function(string, pattern) {
+  str_detect(string, regex(pattern, ignore_case = TRUE))
+}
+
+str_ext <- function(string, pattern) {
+  str_extract(string, regex(pattern, ignore_case = TRUE))
+}
+
+str_spl <- function(string, pattern) {
+  str_split(string, regex(pattern, ignore_case = TRUE))
+}
+
+
+
 
 
 
---
+++ scratchpad/import-data.R
@@ -0,0 +1,92 @@
+source(here::here("code/setup.R"))
+library("googledrive")
+library(googlesheets4)
+
+gs4_auth(email = "devin.jl@gmail.com")
+drive_auth(email = "devin.jl@gmail.com")
+
+# get all org comments 
+s <- drive_find(pattern = "org_comments")
+
+s$name %>% kable
+
+# read all as char
+read_sheet_c <- . %>% read_sheet(col_types = "c")
+
+# init
+d1 <- read_sheet_c(s[1,])
+
+# map
+d <- map_dfr(s$id, possibly(read_sheet_c, otherwise = head(d1)))
+
+# class
+d %<>%
+  na_if("NA") %>% 
+  mutate(docket_id = document_id %>% str_extract("[A-Z]*-[0-9]*-[0-9]*"))# %>% 
+  mutate(across(starts_with("success")), as.numeric )
+
+d$position %<>% as.numeric()
+  
+# docket - level summary vars
+d %<>%
+  na_if("NA") %>% 
+  drop_na(position) %>% 
+  group_by(docket_id) %>%
+  mutate(coalitions = unique(coalition_comment) %>% length(),
+         positions = unique(position) %>% length(),
+         coalition_unopposed = abs(max(position, na.rm = T)-min(position,na.rm = T)) < 2,
+         coalition_type = tolower(coalition_type),
+         comments = as.numeric(number_of_comments_received) %>% replace_na(1),
+         congress = str_dct(org_type, "congress"))
+
+d$comments %>% range(na.rm = T)
+d$position %>% unique()
+d$positions %>% unique()
+d$coalition_unopposed %>% unique()
+d$congress %>% unique()
+
+#FIXME just to make sure we are not dropping obs due to this reasonable assumption
+d$congress %<>% replace_na(FALSE)
+
+d_coded <- d %>% drop_na(position)
+
+d_all <- d
+d <- d_all
+
+d_coalitions <- d_coded %>% 
+  ungroup() %>% 
+  drop_na(coalition_comment) %>% 
+  group_by(docket_id, coalition_comment, coalition_unopposed, coalitions) %>% 
+  summarise(cong_support = sum(congress),
+            coalition_size = n(),
+            coalition_business = str_dct(coalition_type, "private") %>% unique(),
+            coalition_success = mean(success %>% as.numeric(), na.rm = T), # average success of coalition
+            comments = sum(comments)) %>%
+  ungroup() %>% 
+  mutate(coalition_id = as.factor(coalition_comment) %>% as.numeric())
+
+d_coalitions %>% kablebox()
+
+# diognostics 
+d_all %>% ungroup() %>%
+  drop_na(docket_id) %>% 
+  group_by(docket_id) %>% 
+  distinct(coalition_comment, coalition_type) %>% 
+  add_count(coalition_comment) %>% 
+  arrange(coalition_comment) %>% 
+  filter(n > 1) %>% kablebox()
+
+coalitions_coded <- d_coalitions 
+
+save(coalitions_coded, file = here::here("data", "coalitions_coded.Rdata"))
+
+ggplot(coalitions_coded, aes(x = coalition_success)) + geom_histogram()+ labs(x = "Coalition Success")
+
+ggplot(coalitions_coded, aes(x = coalition_business %>% as.numeric())) + geom_histogram()+ labs(x = "Business Coalition")
+
+ggplot(coalitions_coded, aes(x = coalition_size)) + geom_histogram()+ labs(x = "Coalition size")
+
+#ggplot(coalitions_coded, aes( x= comment_length)) + geom_histogram()+ labs(x = "% (Comment length/proposed rule length)*100")
+
+ggplot(coalitions_coded, aes( x= log(comments))) + geom_histogram() + labs(x = "Log(comments)")
+
