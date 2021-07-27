1cf31c9e84d86f7c86ad5697d9f98332bbeb48a2
judgelord <devin.jl@gmail.com>
2021-07-27 16:09:57 -0500

    fix citations

--- code/update_codebook.R
+++ code/update_codebook.R
@@ -1,4 +1,8 @@
-drive_get(id ="1o1hi0z9O-G9xsgkspOFG2VWzh0wQKjiezzoVpItaCxU") %>%
-  drive_download(path = here::here("99-codebook.txt"), overwrite = T)
+library(googledrive)
 
-file.rename("99-codebook.txt", "99-codebook.md")
+drive_get(id = "1o1hi0z9O-G9xsgkspOFG2VWzh0wQKjiezzoVpItaCxU") %>%
+  drive_download(path = here::here("99-codebook.txt"), 
+                 overwrite = T)
+
+file.rename("99-codebook.txt", 
+            "99-codebook.md")
