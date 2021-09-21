8fd71df4a41e932b79585b9f98667406068b7642
judgelord <devin.jl@gmail.com>
2021-09-20 22:14:30 -0400

    update race fig in ej pres

---
+++ code/make-article.R
@@ -0,0 +1,26 @@
+# Test
+# chapter <- "05-ej/ej.tex"
+
+make_article <- function(chapter){
+
+text <- readLines(here::here(chapter)) %>%
+  # up one header level
+  str_replace_all("subsection", "section") %>% 
+  # inspect for chapter refs 
+  #.[doc %>% str_detect("(C|c)hapter")] %>% 
+  # change chapter refs 
+  str_replace_all("this chapter", "this paper") %>% 
+  str_replace_all("This chapter", "This paper") %>% 
+  str_replace_all("(In the |The) previous chapter(s|)", "Elsewhere") %>% 
+  str_replace_all("(in )the previous chapter(s|)", "elsewhere") %>% 
+  str_replace_all("Chapter .ref", "\\\\citet")
+
+# make article file path
+article <- here::here(chapter) %>% str_replace("05-ej", "docs")# str_replace(".tex$", "-article.tex") 
+
+# save article version of tex file 
+writeLines(text, article)  
+
+# compile tex
+tinytex::xelatex(article)
+}
